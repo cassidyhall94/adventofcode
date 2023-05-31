ff7306c73b6faf7ddfda2df1d0e81862865b73d5
cassidy.hall94 <cassidy.hall94@gmail.com>
2023-05-31 16:20:35 +0100

    day2 complete

--- main.go
+++ main.go
@@ -1,55 +1,26 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
 	"sort"
-	"strconv"
 )
 
-//Find the Elf carrying the most Calories. How many total Calories is that Elf carrying?
-//parse output file
-// sum the grouped numbers together, seperate totals on a newline
-// range over numbers to find the highest and return
-
 func main() {
-	file, err := os.Open("output.txt")
-	if err != nil {
-		fmt.Println("file open error: %w", err)
-	}
-	defer file.Close()
-
-	scanner := bufio.NewScanner(file)
-	cur := []int{}
-	ret := [][]int{}
-
-	for scanner.Scan() {
-		if len(scanner.Text()) > 0 {
-			convStr, err := strconv.Atoi(scanner.Text())
-			if err != nil {
-				fmt.Println("strconv error: %w", err)
-			}
-			cur = append(cur, convStr)
-		}
-
-		if len(scanner.Text()) == 0 {
-			ret = append(ret, cur)
-			cur = []int{}
-		}
-	}
-
-	if err := scanner.Err(); err != nil {
-		fmt.Println("file scanner error: %w", err)
-	}
-	addSums(ret)
+	// ret_day1 := readFile("day1_input.txt")
+	ret_day2 := readFile2("day2_input.txt")
+	// totalCalories := day1_addSums(ret_day1)
+	totalScore := day2_addScores(ret_day2)
+	totalNewScore := day2_addNewScores(ret_day2)
+	// fmt.Println(totalCalories)
+	fmt.Println(totalScore)
+	fmt.Println(totalNewScore)
 }
 
-func addSums(ret [][]int) int {
+func day1_addSums(ret_day1 [][]int) int {
 	totals := []int{}
 	total := 0
 
-	for _, elfSnacks := range ret {
+	for _, elfSnacks := range ret_day1 {
 		for i := 0; i < len(elfSnacks); i++ {
 			total += elfSnacks[i]
 		}
@@ -57,6 +28,126 @@ func addSums(ret [][]int) int {
 		total = 0
 	}
 	sort.Sort(sort.Reverse(sort.IntSlice(totals)))
-	fmt.Println(totals[0])
+	// topThreeCalories := day1_addTopThree(totals)
+	// fmt.Println(topThreeCalories)
 	return totals[0]
+}
+
+func day1_addTopThree(summedCalories []int) int {
+	total := 0
+
+	for i, elfSnacks := range summedCalories {
+		if i < 3 {
+			total += elfSnacks
+		}
+	}
+	return total
+}
+
+func day2_addScores(ret_day2 [][]string) int {
+	total := 0
+	paper := 2
+	rock := 1
+	scissors := 3
+
+	lose := 0
+	draw := 3
+	win := 6
+
+	for _, round := range ret_day2 {
+		for _, turn := range round {
+			for _, char := range turn {
+				if char != 32 {
+					if turn[0] == 65 {
+						if string(char) == "X" {
+							total += rock + draw
+						}
+						if string(char) == "Y" {
+							total += paper + win
+						}
+						if string(char) == "Z" {
+							total += scissors + lose
+						}
+					}
+					if turn[0] == 66 {
+						if string(char) == "X" {
+							total += rock + lose
+						}
+						if string(char) == "Y" {
+							total += paper + draw
+						}
+						if string(char) == "Z" {
+							total += scissors + win
+						}
+					}
+					if turn[0] == 67 {
+						if string(char) == "X" {
+							total += rock + win
+						}
+						if string(char) == "Y" {
+							total += paper + lose
+						}
+						if string(char) == "Z" {
+							total += scissors + draw
+						}
+					}
+				}
+			}
+		}
+	}
+	return total
+}
+
+func day2_addNewScores(ret_day2 [][]string) int {
+	total := 0
+	paper := 2
+	rock := 1
+	scissors := 3
+
+	lose := 0
+	draw := 3
+	win := 6
+
+	for _, round := range ret_day2 {
+		for _, turn := range round {
+			for _, char := range turn {
+				if char != 32 {
+					if turn[0] == 65 {
+						if string(char) == "X" {
+							total += scissors + lose
+						}
+						if string(char) == "Y" {
+							total += rock + draw
+						}
+						if string(char) == "Z" {
+							total += paper + win
+						}
+					}
+					if turn[0] == 66 {
+						if string(char) == "X" {
+							total += rock + lose
+						}
+						if string(char) == "Y" {
+							total += paper + draw
+						}
+						if string(char) == "Z" {
+							total += scissors + win
+						}
+					}
+					if turn[0] == 67 {
+						if string(char) == "X" {
+							total += paper + lose
+						}
+						if string(char) == "Y" {
+							total += scissors + draw
+						}
+						if string(char) == "Z" {
+							total += rock + win
+						}
+					}
+				}
+			}
+		}
+	}
+	return total
 }
---
+++ utils.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func readFile(filename string) [][]int {
+	file, err := os.Open(filename)
+	if err != nil {
+		fmt.Println("file open error: %w", err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	cur := []int{}
+	ret := [][]int{}
+
+	for scanner.Scan() {
+		if len(scanner.Text()) > 0 {
+			convStr, err := strconv.Atoi(scanner.Text())
+			if err != nil {
+				fmt.Println("strconv error: %w", err)
+			}
+			cur = append(cur, convStr)
+		}
+
+		if len(scanner.Text()) == 0 {
+			ret = append(ret, cur)
+			cur = []int{}
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Println("file scanner error: %w", err)
+	}
+	return ret
+}
+
+func readFile2(filename string) [][]string {
+	file, err := os.Open(filename)
+	if err != nil {
+		fmt.Println("file open error: %w", err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	cur := []string{}
+	ret := [][]string{}
+
+	for scanner.Scan() {
+		if len(scanner.Text()) > 0 {
+			cur = append(cur, scanner.Text())
+		}
+		ret = append(ret, cur)
+		cur = []string{}
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Println("file scanner error: %w", err)
+	}
+	return ret
+}
+
+// func addSums(scores []int) int {
+// 	total := 0
+
+// 	for i := 0; i < len(scores); i++ {
+// 		total += scores[i]
+// 	}
+// 	return total
+// }
